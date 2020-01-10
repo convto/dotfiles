1069a43236bb8087580c8baae16d882b27231660
srttk <saroteki26@gmail.com>
2020-01-10 18:21:06 +0900

    gitconfig追加

--- sh/link.sh
+++ sh/link.sh
@@ -5,3 +5,5 @@ ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
